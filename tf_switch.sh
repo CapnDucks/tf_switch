cf874b5e629f007216bf854a272f028bf31303c3
Cap'n Duck <42878045+CapnDucks@users.noreply.github.com>
2022-10-04 11:18:23 -0700

    Fixed usage and current_version variable (#5)
    Co-authored-by: Capn Duck <git@capnduck.com>

--- tf_switch.sh
+++ tf_switch.sh
@@ -7,6 +7,7 @@
 BASE_URL="https://releases.hashicorp.com/terraform"
 CHECK_URL="https://checkpoint-api.hashicorp.com/v1/check/terraform"
 CURL=$(command -v curl)
+CURRENT_VERSION=$(terraform --version 2> /dev/null | head -n 1)
 JQ=$(command -v jq)
 KERNEL=$(uname -s | tr "[:upper:]" "[:lower:]")
 NUM_RELEASES=${2:-25}
@@ -14,11 +15,15 @@ TEMP_DIR=$(mktemp -d)
 TF_PATH="$(command -v terraform | cut -d\/ -f1-4)"
 TF_VERSION="${1}"
 UNZIP=$(command -v unzip)
-USAGE="Usage: tf_switch {version you'd like to use}: eg. 'tf_switch 0.14.11'\n Or you can specify 'latest' to get the latest version\n If you are unsure what versions are available, you can use 'list {number}' to get a list of terraform releases (defaults to 25)"
+USAGE="Usage: tf_switch {version you'd like to use}: eg. 'tf_switch 0.14.11'\n Or you can specify 'latest' to get the latest version\n If you are unsure what versions are available, you can use 'list {number}' to get a list of terraform releases (defaults to 25)\n"
 
 if [ -z ${CURL} ]; then
   printf "curl is not installed or is not in your PATH.  Please rectify the error.\n"
   exit 1
+fi
+
+if [ -z $CURRENT_VERSION ]; then
+  CURRENT_VERSION="no current version installed!"
 fi
 
 if [ -z ${JQ} ] && [ ${TF_VERSION} = "latest" ]; then
@@ -33,6 +38,7 @@ fi
 
 if [ -z "${TF_VERSION}" ]; then
   printf "${USAGE}"
+  printf "You are currently using: $CURRENT_VERSION\n"
   exit 0 # Not an error to ask for help
 fi
 
